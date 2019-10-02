3091c7cc3eaa95ee17dc156f194a26b25a282fcc
David Gassner <dgassnerdev@gmail.com>
2019-10-02 13:06:17 -0700

    04_07

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String s1 = "Hello!";
+        var s2 = "Hello!";
     }
 }
