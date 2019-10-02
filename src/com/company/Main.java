1b086c98ae645bb413006b548278185818c4688a
David Gassner <dgassnerdev@gmail.com>
2019-10-02 10:21:06 -0700

    04_04e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,22 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        var result2 = 10 + 20;
+
+        int intValue = 42;
+        var fromInt = Integer.toString(intValue);
+        System.out.println(fromInt);
+
+        boolean boolValue = true;
+        var fromBool = Boolean.toString(boolValue);
+        System.out.println(fromBool);
+
+        long longValue = 10_000_000;
+        var fromLong = Long.toString(longValue);
+        System.out.println(fromLong);
     }
 }
