1ea696e3923d43d26d74b9a34f02fae9a8585130
David Gassner <dgassnerdev@gmail.com>
2019-10-02 09:59:50 -0700

    04_02e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var sb = new StringBuilder("Welcome");
+        sb.append(" to California!");
+        var s = sb.toString();
+        StringBuilder b = new StringBuilder();
+        b.append("Shirt size: ")
+                .append("M")
+                .append(", Qty: ")
+                .append(4);
+        var s2 = b.toString();
+        System.out.println(s2);
     }
 }
