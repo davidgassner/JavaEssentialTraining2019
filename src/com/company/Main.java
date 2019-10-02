895140f029e7eceb089dd13a0696b35af391fe98
David Gassner <dgassnerdev@gmail.com>
2019-10-02 14:05:30 -0700

    04_08e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         var s1 = "Welcome to California!";
+        System.out.println("length of string: " + s1.length());
+
+        int position = s1.indexOf("California");
+        System.out.println("The position is " + position);
+
+        var sub = s1.substring(11);
+        System.out.println(sub);
+
+        String s2 = "Welcome!      ";
+        var len = s2.length();
+        System.out.println(len);
+        var trimmed = s2.trim();
+        System.out.println(trimmed.length());
     }
 }
