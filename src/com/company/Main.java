f590a74d28396d47a7ec0ad7b72703fbce3c014c
David Gassner <dgassnerdev@gmail.com>
2019-10-02 14:16:53 -0700

    04_09e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
 
+        System.out.print("Enter a value: ");
+        var input = scanner.nextLine();
+        System.out.println(input);
+
+        System.out.print("Enter number 1: ");
+        var number1 = scanner.nextInt();
+        System.out.print("Enter number 2: ");
+        var number2 = scanner.nextInt();
+
+        var sum = number1 + number2;
+        System.out.println("The sum is " + sum);
     }
 }
