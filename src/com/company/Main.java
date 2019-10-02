0c9610aee76ac77d8c30d8e5001afe38934e2367
David Gassner <dgassnerdev@gmail.com>
2019-10-02 13:31:53 -0700

    04_07e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,33 @@ public class Main {
     public static void main(String[] args) {
         String s1 = "Hello!";
         var s2 = "Hello!";
+
+        if (s1 == s2) {
+            System.out.println("they match!");
+        } else {
+            System.out.println("they don't match!");
+        }
+
+        String s3 = new String("Hello!");
+        String s4 = new String("Hello!");
+        if (s3 == s4) {
+            System.out.println("they match!");
+        } else {
+            System.out.println("they don't match!");
+        }
+
+        if (s3.equals(s4)) {
+            System.out.println("they match!");
+        } else {
+            System.out.println("they don't match!");
+        }
+
+        var s5 = "HELLO!";
+        if (s3.equalsIgnoreCase(s5)) {
+            System.out.println("they match!");
+        } else {
+            System.out.println("they don't match!");
+        }
+
     }
 }
