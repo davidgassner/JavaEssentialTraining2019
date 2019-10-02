2b6b598b4649042ed6b730d6c87bfd9bef7a096e
David Gassner <dgassnerdev@gmail.com>
2019-10-02 14:19:52 -0700

    04_11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var sc = new Scanner(System.in);
+
+        System.out.print("Enter a numeric value: ");
+        var d1 = sc.nextDouble();
+
+        System.out.print("Enter a numeric value: ");
+        var d2 = sc.nextDouble();
+
+        double result = d1 / d2;
+
+        System.out.println("The answer is " + result);
     }
+
 }
+
