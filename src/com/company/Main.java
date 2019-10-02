589bbacfbb8cae00b573a75832f7756f88202694
David Gassner <dgassnerdev@gmail.com>
2019-10-02 10:05:31 -0700

    04_04

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         int result = 10 + 12;
         String answer = "The answer is " + result;
+        System.out.println(answer);
     }
 }
