bc814de262421277d56f7f995c7bc06d0e8bc9cb
David Gassner <dgassnerdev@gmail.com>
2019-10-02 10:01:40 -0700

    04_04

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int result = 10 + 12;
+        String answer = "The answer is " + result;
     }
 }
