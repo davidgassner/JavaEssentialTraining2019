618145ee0e96cabef1b2b0df34eed26f6dcbac29
David Gassner <dgassnerdev@gmail.com>
2019-10-02 10:55:11 -0700

    04_05e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
         var doubleValue = 10_000_000.53;
+
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number: " + numF.format(doubleValue));
+
+        var intF = NumberFormat.getIntegerInstance();
+        System.out.println("Number: " + intF.format(doubleValue));
+
+        intF.setGroupingUsed(false);
+        System.out.println("Number: " + intF.format(doubleValue));
+
+        var locale = new Locale("de", "DE");
+        var localeFormatter = NumberFormat.getNumberInstance(locale);
+        System.out.println("Number: " + localeFormatter.format(doubleValue));
+
+        var currencyFormatter = NumberFormat.getCurrencyInstance();
+        System.out.println(currencyFormatter.format(doubleValue));
+
+        var df = new DecimalFormat("$00.00");
+        System.out.println(df.format(1));
+
     }
 }
