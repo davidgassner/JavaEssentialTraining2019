b3d233bade7cc99c86b8b00c951413396e303d23
David Gassner <dgassnerdev@gmail.com>
2019-10-03 10:16:26 -0700

    05_01e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,30 @@
 package com.company;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var scanner = new Scanner(System.in);
-        System.out.print("Enter a month number (1-12): ");
-        var monthNumber = scanner.nextInt();
+//        var scanner = new Scanner(System.in);
+//        System.out.print("Enter a month number (1-12): ");
+//        var monthNumber = scanner.nextInt();
 
+        var now = LocalDateTime.now();
+        var monthNumber = now.getMonthValue();
+
+        String message;
+//        if (monthNumber < 1 || monthNumber > 12) {
+//            message = "That isn't a valid month!";
+//        } else
+        if (monthNumber <= 3) {
+            message = "That's in Quarter 1!";
+        } else if (monthNumber <= 6) {
+            message = "That's in Quarter 2!";
+        } else {
+            message = "That isn't the first half of the year";
+        }
+        System.out.println(message);
     }
 }
