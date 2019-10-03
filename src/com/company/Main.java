87162eafe80ba6257cd1b7e1ff28fd1e438d89b1
David Gassner <dgassnerdev@gmail.com>
2019-10-03 10:39:10 -0700

    05_02e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,24 @@ public class Main {
         var now = LocalDateTime.now();
         var monthNumber = now.getMonthValue();
 
+        switch (monthNumber) {
+            case 1:
+                System.out.println("The month is January");
+                break;
+            case 2:
+                System.out.println("The month is February");
+                break;
+            case 3:
+                System.out.println("The month is March");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("It's the 4th quarter");
+                break;
+            default:
+                System.out.println("You chose another month");
+        }
+
     }
 }
