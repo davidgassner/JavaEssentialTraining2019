05d2301eeb68c3ca3709ca050e028731a8824015
David Gassner <dgassnerdev@gmail.com>
2019-10-03 15:11:53 -0700

    06_02e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String s = null;
+        try {
+            var sub = s.substring(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("I'm not dead yet!");
     }
 }
