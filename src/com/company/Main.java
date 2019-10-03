ebac0dde30f7a4e5c397f2d97622b13414a0ac81
David Gassner <dgassnerdev@gmail.com>
2019-10-03 13:49:41 -0700

    05_05e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,12 @@ public class Main {
 
         double result = addValues(i1, i2);
         System.out.println("The sum is " + result);
+
+        double resultOfFour = addValues(i1, i2, i1, i2);
+        System.out.println("The sum of 4 is " + resultOfFour);
+
+        double resultOfMultiple = addValues(i1, i2, i1, i2, i1, i2);
+        System.out.println("The sum of multiple is " + resultOfMultiple);
     }
 
     private static int getInput(Scanner sc, String prompt) {
@@ -20,6 +26,18 @@ public class Main {
 
     private static double addValues(int i1, int i2) {
         return i1 + i2;
+    }
+
+    private static double addValues(int i1, int i2, int i3, int i4) {
+        return i1 + i2 + i3 + i4;
+    }
+
+    private static double addValues(int... values) {
+        int result = 0;
+        for (var value : values) {
+            result += value;
+        }
+        return result;
     }
 
 }
