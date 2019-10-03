23a163a8e56970edf782d8a43f16c65a8e181734
David Gassner <dgassnerdev@gmail.com>
2019-10-03 11:11:46 -0700

    05_03e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,25 @@ public class Main {
                         "July", "August", "September",
                         "October", "November", "December"};
 
+//        for (int i = 0; i < months.length; i++) {
+//            System.out.println(months[i]);
+//        }
+
+//        for (var month : months) {
+//            System.out.println(month);
+//        }
+
+//        var whileCounter = 0;
+//        while (whileCounter < months.length) {
+//            System.out.println(months[whileCounter]);
+//            whileCounter++;
+//        }
+
+        var doCounter = 0;
+        do {
+            System.out.println(months[doCounter]);
+            doCounter++;
+        } while (doCounter < months.length);
+
     }
 }
