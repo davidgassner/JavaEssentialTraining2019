c2708f738d8a90252024215ced00bbff391a5496
David Gassner <dgassnerdev@gmail.com>
2019-10-03 10:12:33 -0700

    05_01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter a month number (1-12): ");
+        var monthNumber = scanner.nextInt();
+
     }
 }
