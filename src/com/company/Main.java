064b3c89347934552013810380c4c9355e359db2
David Gassner <dgassnerdev@gmail.com>
2019-10-06 11:52:16 -0700

    06_03e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s = null;
+        String s = "";
         try {
             var sub = s.substring(1);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("Null pointer: " + e.getMessage());
+        } catch (StringIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("Out of bounds: " + e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("I don't know what happened: " + e.getMessage());
         }
         System.out.println("I'm not dead yet!");
     }
