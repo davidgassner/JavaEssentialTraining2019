4e40e38a1ecd5965fa8c22308fafc642650c7627
David Gassner <dgassnerdev@gmail.com>
2019-10-06 12:29:36 -0700

    06_04e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,14 @@ public class Main {
     public static void main(String[] args) {
         var file = new File("hello.txt");
         System.out.println("File exists: " + file.exists());
+
+        try (FileReader reader = new FileReader(file);
+             BufferedReader br = new BufferedReader(reader)) {
+            var text = br.readLine();
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
