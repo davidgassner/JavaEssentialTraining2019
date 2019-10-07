5e32937cc70ea00ebb970aba5af4f6ad0b200e06
David Gassner <dgassnerdev@gmail.com>
2019-10-07 14:57:06 -0700

    original commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,8 @@
 package com.company;
 
-import com.company.model.ClothingSize;
-import com.company.model.Shirt;
-
-import java.text.NumberFormat;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        var item = new Shirt(
-                ClothingSize.L,
-                19.99,
-                3);
-
-        var totalPrice = item.getPrice() * item.getQuantity();
-        var formatter = NumberFormat.getCurrencyInstance();
-        var output = String.format("Your %s %s order will cost %s",
-                item.getSize(),
-                item.getType(),
-                formatter.format(totalPrice));
-        System.out.println(output);
+	// write your code here
     }
 }
--- src/com/company/model/Shirt.java
+++
@@ -1,8 +0,0 @@
-package com.company.model;
-
-public class Shirt extends ClothingItem {
-
-    public Shirt(ClothingSize size, double price, int quantity) {
-        super(ClothingItem.SHIRT, size, price, quantity);
-    }
-}
