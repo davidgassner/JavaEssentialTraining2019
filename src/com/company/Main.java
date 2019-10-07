c03d6546de314edbe2c4ef4255c1d20c085b5caa
David Gassner <dgassnerdev@gmail.com>
2019-10-07 10:15:38 -0700

    07_03e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import com.company.model.ClothingItem;
+
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var item = new ClothingItem();
+        item.setType("Shirt");
+        item.setSize("M");
+        item.setPrice(19.99);
+        item.setQuantity(3);
+
+        var totalPrice = item.getPrice() * item.getQuantity();
+        var formatter = NumberFormat.getCurrencyInstance();
+        var output = String.format("Your %s order will cost %s",
+                item.getType(),
+                formatter.format(totalPrice));
+        System.out.println(output);
     }
 }
---
+++ src/com/company/model/ClothingItem.java
@@ -0,0 +1,40 @@
+package com.company.model;
+
+public class ClothingItem {
+    private String type;
+    private String size;
+    private double price;
+    private int quantity;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
