64457fd09495edc6f170bb9d1327ac86a4a44b8e
David Gassner <dgassnerdev@gmail.com>
2019-10-07 09:35:17 -0700

    07_03

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,8 @@
 package com.company;
 
-import com.company.model.Person;
-
 public class Main {
 
     public static void main(String[] args) {
-        var person = new Person();
-        person.setFirstName("Joe");
-        person.setLastName("Smith");
-        person.setAge(42);
-        var output = String.format("%s %s (%d years old)",
-                person.getFirstName(),
-                person.getLastName(),
-                person.getAge());
-        System.out.println(output);
+	// write your code here
     }
 }
