b2e9cbd9e02d1f156f69038f23313237045b1595
peteryin123 <peteryin123@gmail.com>
2020-11-11 11:43:29 -0800

    My changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int result = 10 + 12;
+        int result = 10 * 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
     }
