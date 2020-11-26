e1c9b132b110e83aad9cb1aa1450fe40fc50f649
Karthick-M-18 <karthickmurugesan18@gmail.com>
2020-11-26 12:34:49 +0530

    My changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,17 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        double dec = 100.8787;
+        String double_dec = Double.toString(dec);
+        System.out.println(double_dec);
+
+        int a = 15;
+        String int_a = Integer.toString(a);
+        System.out.println(int_a);
+
+        long b = 109999999;
+        String long_b = Long.toString(b);
+        System.out.println(long_b);
     }
 }
