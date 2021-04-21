ef1ec9af5d3fc81879dd87300a21b1e6de5caba4
volihor <rjcmrf_2005>
2021-04-21 20:05:58 +0300

    ADD print "=== 04_04_GitHub_TestingBranch ==="

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("=== 04_04_GitHub_TestingBranch ===");
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
