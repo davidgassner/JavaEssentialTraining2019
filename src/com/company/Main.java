060f5613d6d7230d31371d4d32fca87d8d8da295
Brandon Blackwell <brandonb13195@gmail.com>
2023-10-06 20:43:40 -0700

    Testing out Java's way of converting primitive types to a string.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,14 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
-    }
+
+        String mString = "This is an int converted to a string: " + 35;
+        System.out.println(mString);
+
+        double mDouble = 654.489;
+        var doubleTostring = Double.toString(mDouble);
+
+        var res = "Converting to a string again " + doubleTostring;
+        System.out.println(res);
+      }
 }
